e484d1752791109d5d2c4a73c4c661ed548a39db
Artem Shchirov <artemshchirov@gmail.com>
2022-12-08 17:00:45 +0200

    refactor(folders): structure starter folders

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import './index.css';
+import './assets/index.css';
 
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
